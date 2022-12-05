b14c70f1fcb25ed1bdbab1f993f0002d455bff8f
Cliff Gallagher <cgallagher@cvent.com>
2022-12-05 14:19:55 -0600

    React logs people from database to console

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState();
+
+  const getInfo = async () => {
+    const response = await fetch('/person');
+    const data = await response.json();
+    console.log(data);
+    return data;
+  }
+
+  useEffect(() => {
+    const data = getInfo();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          ${data}
         </p>
         <a
           className="App-link"
