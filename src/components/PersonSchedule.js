3f4d4a440027cd85cdbf84331b244a046d2b50d1
Cliff Gallagher <cgallagher@cvent.com>
2022-12-07 17:19:23 -0600

    PersonSchedule Delete button works with static value

--- src/components/PersonSchedule.js
+++ src/components/PersonSchedule.js
@@ -2,13 +2,29 @@ import React, {useEffect, useState} from 'react';
 import './PersonSchedule.css';
 
 const PersonSchedule = (props) => {
+
+    const personScheduleDeleteHandler = async (event) => {
+        event.preventDefault();
+        const body = {
+            externalId: 60
+        }
+        const response = await fetch('/personschedule', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+    }
+
     return(
-        <div className="person-schedule">
+        <form className="person-schedule" onSubmit={personScheduleDeleteHandler}>
             <p>Person: {props.personName}</p>
             <p>Task: {props.taskDescription}</p>
             <p>Start: {props.startTime}</p>
             <p>End: {props.endTime}</p>
-        </div>
+            <button type="submit">Delete</button>
+        </form>
     )
 }
 
