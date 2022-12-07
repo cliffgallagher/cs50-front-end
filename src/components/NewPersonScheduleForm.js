315656437064c6ad431d5cdf71fc3bcfc1aaa769
Cliff Gallagher <cgallagher@cvent.com>
2022-12-07 09:47:18 -0600

    Assigned To field of NewPersonScheduleForm correctly passes value to database

--- src/components/NewPersonScheduleForm.js
+++ src/components/NewPersonScheduleForm.js
@@ -6,6 +6,7 @@ import PersonOption from './PersonOption';
 const NewPersonScheduleForm = () => {
     const [taskOptions, setTaskOptions] = useState();
     const [personOptions, setPersonOptions] = useState();
+    const [personInputValue, setPersonInputValue] = useState();
 
     const getTaskOptions = async () => {
         const response = await fetch('/task/unassigned');
@@ -19,19 +20,43 @@ const NewPersonScheduleForm = () => {
         setPersonOptions(data.map(person => <PersonOption externalId={person.externalId} name={person.name} key={person.externalId}/>));
     }
 
+    const newPersonScheduleFormSubmitHandler = async (event) => {
+       event.preventDefault();
+        console.log("personInputValue in submit handler: " + personInputValue);
+        const body = {
+            personId: personInputValue,
+            taskId: 1,
+            startTime: "2022-12-15T06:00:00",
+            endTime: "2022-12-15T07:00:00"
+        }
+        const response = await fetch('/personschedule', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+        return response.json();
+    }
+
+    const personInputChangeHandler = (event) => {
+        //console.log(event.target.value);
+        setPersonInputValue(event.target.value);
+    }
+
     useEffect(() => {
         getTaskOptions();
         getPersonOptions();
     }, [])
 
     return(
-        <form>
+        <form onSubmit={newPersonScheduleFormSubmitHandler}>
             <label for='tasks'>Task: </label>
             <select name='tasks' id='tasks'>
                 {taskOptions}
             </select>
             <label for='person'>Assigned To: </label>
-            <select name='person' id='person'>
+            <select name='person' id='person' onChange={personInputChangeHandler} value={personInputValue}>
                 {personOptions}
             </select>
             <button type='submit'>Assign Person to Task</button>
